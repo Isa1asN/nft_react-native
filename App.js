7c584d6e1dc5e8a88b37904c04be6261d083d02b
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-11 18:33:04 -0700

    added changes

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { useFonts } from "expo-font";
+// import { useFonts } from "expo-font";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
 const Stack = createStackNavigator()
 const theme = {
@@ -12,6 +14,8 @@ const theme = {
 }
 
 export default function App() {
+  // const [loaded] = useFonts({})
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{headerShown : false}} initialRouteName="home" >
