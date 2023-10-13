6bd8b32099560495f908a3e95847eddc60910e3a
Esayas Nigussie <esayasnigusie2@gmail.com>
2023-10-12 21:39:59 -0700

    made changes

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -40,9 +40,15 @@ const HomeHeader = () => {
           </Text>
         </View>
         <View style={{marginTop:SIZES.font}}>
-          <View>
+          <View style={{width:'100%', borderRadius:SIZES.font, backgroundColor:COLORS.gray,flexDirection:'row', alignItems:'center', paddingHorizontal:SIZES.font, paddingVertical:SIZES.small - 2}}>
             <Image source={assets.search} resizeMode='contain' style={{width:20, height:20, marginRight:SIZES.base}}/>
-            <TextInput />
+            <TextInput 
+              placeholder='Search NFTs'
+              style={{
+                flex : 1,
+              }}
+              onChangeText={()=>{}}
+            />
           </View>
         </View>
     </View>
